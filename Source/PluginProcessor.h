0bd1dc6d8c9bcfbd87d1703401c9fef4fc733e13
Weston Clark <westoclark@gmail.com>
2025-10-02 10:41:01 -0700

    Create base classes for DSP

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -57,21 +57,34 @@ public:
   void getStateInformation(juce::MemoryBlock &destData) override;
   void setStateInformation(const void *data, int sizeInBytes) override;
 
-  enum class DSP_Option {
-    Phase,
-    Chorus,
-    Overdrive,
-    LadderFilter,
-    END_OF_LIST
+  enum class DSP_Option { Phase, Chorus, Overdrive, LadderFilter, END_OF_LIST };
+
+  using DSP_Order =
+      std::array<DSP_Option, static_cast<size_t>(DSP_Option::END_OF_LIST)>;
+
+private:
+  DSP_Order dspOrder;
+
+  template <typename DSP> struct DSP_Choice : juce::dsp::ProcessorBase {
+    void prepare(const juce::dsp::ProcessSpec &spec) override {
+      dsp.prepare(spec);
+    }
+    void process(const juce::dsp::ProcessContextReplacing<float> &context) override {
+      dsp.process(context);
+    }
+    void reset() override {
+      dsp.reset();
+    }
+    DSP dsp;
   };
+  
+  DSP_Choice<juce::dsp::DelayLine<float>> delay;
+  DSP_Choice<juce::dsp::Phaser<float>> phaser;
+  DSP_Choice<juce::dsp::Chorus<float>> chorus;
+  DSP_Choice<juce::dsp::LadderFilter<float>> overdrive, ladderFilter;
+  
+  using DSP_Pointers = std::array<juce::dsp::ProcessorBase*,static_cast<size_t>(DSP_Option::END_OF_LIST)>;
 
-  private :
-    juce::dsp::Phase<float> phaser;
-    juce::dsp::Chorus<float> chorus;
-    juce::dsp::LadderFilter<float> overdrive, ladderFilter;
-
-
-      //==============================================================================
-      JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
-          MultieffectpluginAudioProcessor)
+  //==============================================================================
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MultieffectpluginAudioProcessor)
 };
