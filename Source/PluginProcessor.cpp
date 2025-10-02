10907e925c8ffbb01a0cb558a81f9d0dae4938e2
Weston Clark <westoclark@gmail.com>
2025-10-02 13:51:10 -0700

    Sort and process DSP blocks

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -12,168 +12,190 @@
 //==============================================================================
 MultieffectpluginAudioProcessor::MultieffectpluginAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+    : AudioProcessor(
+          BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+              .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+      )
 #endif
 {
 }
 
-MultieffectpluginAudioProcessor::~MultieffectpluginAudioProcessor()
-{
-}
+MultieffectpluginAudioProcessor::~MultieffectpluginAudioProcessor() {}
 
 //==============================================================================
-const juce::String MultieffectpluginAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
+const juce::String MultieffectpluginAudioProcessor::getName() const {
+  return JucePlugin_Name;
 }
 
-bool MultieffectpluginAudioProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
+bool MultieffectpluginAudioProcessor::acceptsMidi() const {
+#if JucePlugin_WantsMidiInput
+  return true;
+#else
+  return false;
+#endif
 }
 
-bool MultieffectpluginAudioProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
+bool MultieffectpluginAudioProcessor::producesMidi() const {
+#if JucePlugin_ProducesMidiOutput
+  return true;
+#else
+  return false;
+#endif
 }
 
-bool MultieffectpluginAudioProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
+bool MultieffectpluginAudioProcessor::isMidiEffect() const {
+#if JucePlugin_IsMidiEffect
+  return true;
+#else
+  return false;
+#endif
 }
 
-double MultieffectpluginAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
+double MultieffectpluginAudioProcessor::getTailLengthSeconds() const {
+  return 0.0;
 }
 
-int MultieffectpluginAudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+int MultieffectpluginAudioProcessor::getNumPrograms() {
+  return 1; // NB: some hosts don't cope very well if you tell them there are 0
+            // programs, so this should be at least 1, even if you're not really
+            // implementing programs.
 }
 
-int MultieffectpluginAudioProcessor::getCurrentProgram()
-{
-    return 0;
+int MultieffectpluginAudioProcessor::getCurrentProgram() { return 0; }
+
+void MultieffectpluginAudioProcessor::setCurrentProgram(int index) {}
+
+const juce::String MultieffectpluginAudioProcessor::getProgramName(int index) {
+  return {};
 }
 
-void MultieffectpluginAudioProcessor::setCurrentProgram (int index)
-{
-}
-
-const juce::String MultieffectpluginAudioProcessor::getProgramName (int index)
-{
-    return {};
-}
-
-void MultieffectpluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
+void MultieffectpluginAudioProcessor::changeProgramName(
+    int index, const juce::String &newName) {}
 
 //==============================================================================
-void MultieffectpluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+void MultieffectpluginAudioProcessor::prepareToPlay(double sampleRate,
+                                                    int samplesPerBlock) {
+  // Use this method as the place to do any pre-playback
+  // initialisation that you need..
 }
 
-void MultieffectpluginAudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+void MultieffectpluginAudioProcessor::releaseResources() {
+  // When playback stops, you can use this as an opportunity to free up any
+  // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool MultieffectpluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+bool MultieffectpluginAudioProcessor::isBusesLayoutSupported(
+    const BusesLayout &layouts) const {
+#if JucePlugin_IsMidiEffect
+  juce::ignoreUnused(layouts);
+  return true;
+#else
+  // This is the place where you check if the layout is supported.
+  // In this template code we only support mono or stereo.
+  // Some plugin hosts, such as certain GarageBand versions, will only
+  // load plugins that support stereo bus layouts.
+  if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+      layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+  // This checks if the input layout matches the output layout
+#if !JucePlugin_IsSynth
+  if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+    return false;
+#endif
 
-    return true;
-  #endif
+  return true;
+#endif
 }
 #endif
 
-void MultieffectpluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
-{
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+void MultieffectpluginAudioProcessor::processBlock(
+    juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midiMessages) {
+  juce::ScopedNoDenormals noDenormals;
+  auto totalNumInputChannels = getTotalNumInputChannels();
+  auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+  // In case we have more outputs than inputs, this code clears any output
+  // channels that didn't contain input data, (because these aren't
+  // guaranteed to be empty - they may contain garbage).
+  // This is here to avoid people getting screaming feedback
+  // when they first compile a plugin, but obviously you don't need to keep
+  // this code if your algorithm always overwrites all the output channels.
+  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+    buffer.clear(i, 0, buffer.getNumSamples());
 
+  auto newDSPOrder = DSP_Order();
+  while (dspOrderFifo.pull(newDSPOrder))
+
+    // If we pull a change, update the dspOrder
+    if (newDSPOrder != DSP_Order()) {
+      dspOrder = newDSPOrder;
+    }
+
+  // Convert dspOrder into pointers
+  DSP_Pointers dspPointers;
+  for (size_t i = 0; i < dspPointers.size(); ++i) {
+    switch (dspOrder[i]) {
+    case DSP_Option::Phase:
+      dspPointers[i] = &phaser;
+      break;
+    case DSP_Option::Chorus:
+      dspPointers[i] = &chorus;
+      break;
+    case DSP_Option::OverDrive:
+      dspPointers[i] = &overdrive;
+      break;
+    case DSP_Option::LadderFilter:
+      dspPointers[i] = &ladderFilter;
+      break;
+    case DSP_Option::END_OF_LIST:
+      jassertfalse;
+      break;
+    }
+  }
+
+  // Process
+  auto block = juce::dsp::AudioBlock<float>(buffer);
+  auto context = juce::dsp::ProcessContextReplacing<float>(block);
+
+  for (size_t i = 0; i < dspPointers.size(); ++i) {
+    if (dspPointers[i] != nullptr) {
+      dspPointers[i]->process(context);
+    }
+  }
+}
+//==============================================================================
+bool MultieffectpluginAudioProcessor::hasEditor() const {
+  return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor *MultieffectpluginAudioProcessor::createEditor() {
+  return new MultieffectpluginAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-bool MultieffectpluginAudioProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
+void MultieffectpluginAudioProcessor::getStateInformation(
+    juce::MemoryBlock &destData) {
+  // You should use this method to store your parameters in the memory block.
+  // You could do that either as raw data, or use the XML or ValueTree classes
+  // as intermediaries to make it easy to save and load complex data.
 }
 
-juce::AudioProcessorEditor* MultieffectpluginAudioProcessor::createEditor()
-{
-    return new MultieffectpluginAudioProcessorEditor (*this);
-}
-
-//==============================================================================
-void MultieffectpluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-}
-
-void MultieffectpluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+void MultieffectpluginAudioProcessor::setStateInformation(const void *data,
+                                                          int sizeInBytes) {
+  // You should use this method to restore your parameters from this memory
+  // block, whose contents will have been created by the getStateInformation()
+  // call.
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new MultieffectpluginAudioProcessor();
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() {
+  return new MultieffectpluginAudioProcessor();
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -8,8 +8,8 @@
 
 #pragma once
 
-#include <JuceHeader.h>
 #include <Fifo.h>
+#include <JuceHeader.h>
 
 //==============================================================================
 /**
@@ -58,7 +58,7 @@ public:
   void getStateInformation(juce::MemoryBlock &destData) override;
   void setStateInformation(const void *data, int sizeInBytes) override;
 
-  enum class DSP_Option { Phase, Chorus, Overdrive, LadderFilter, END_OF_LIST };
+  enum class DSP_Option { Phase, Chorus, OverDrive, LadderFilter, END_OF_LIST };
 
   using DSP_Order =
       std::array<DSP_Option, static_cast<size_t>(DSP_Option::END_OF_LIST)>;
@@ -71,21 +71,21 @@ private:
     void prepare(const juce::dsp::ProcessSpec &spec) override {
       dsp.prepare(spec);
     }
-    void process(const juce::dsp::ProcessContextReplacing<float> &context) override {
+    void
+    process(const juce::dsp::ProcessContextReplacing<float> &context) override {
       dsp.process(context);
     }
-    void reset() override {
-      dsp.reset();
-    }
+    void reset() override { dsp.reset(); }
     DSP dsp;
   };
-  
+
   DSP_Choice<juce::dsp::DelayLine<float>> delay;
   DSP_Choice<juce::dsp::Phaser<float>> phaser;
   DSP_Choice<juce::dsp::Chorus<float>> chorus;
   DSP_Choice<juce::dsp::LadderFilter<float>> overdrive, ladderFilter;
-  
-  using DSP_Pointers = std::array<juce::dsp::ProcessorBase*,static_cast<size_t>(DSP_Option::END_OF_LIST)>;
+
+  using DSP_Pointers = std::array<juce::dsp::ProcessorBase *,
+                                  static_cast<size_t>(DSP_Option::END_OF_LIST)>;
   //==============================================================================
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MultieffectpluginAudioProcessor)
 };
